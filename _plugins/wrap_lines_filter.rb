c17754545cbc59a76362304aec71275290909807
Dominik Wehr <dominik.wehr@gu.se>
2025-02-07 14:32:53 +0100

    Fix line wrapping

---
+++ _plugins/wrap_lines_filter.rb
@@ -0,0 +1,23 @@
+# From https://talk.jekyllrb.com/t/how-to-fold-wrap-the-content-after-number-of-characters/7312/10
+
+module Jekyll
+  module WrapLinesFilter
+    def wrap_lines(input_string)
+      max_length = 72
+      wrapped_string = ""
+
+      while input_string.length > max_length
+        line = input_string.slice!(0, max_length)
+        line += "\r\n "
+        wrapped_string += line
+      end
+
+      wrapped_string += input_string
+
+      wrapped_string
+
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::WrapLinesFilter)
